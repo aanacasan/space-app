85307dfbaff696e1fed270a3121c980198f6798a
aanacasan <aanacasan@outlook.com>
2024-12-10 12:13:00 -0300

    primeiro commit

---
+++ src/componentes/Galeria/Tags/index.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import tags from "./tags.json"
+
+const TagsContainer =  styled.section`
+    display: flex;
+    gap: 10px;   
+    align-items: center;  
+    margin: 30px 0;   
+    
+`
+
+const TagTitulo =  styled.h3`
+  
+        font-size: 24px;
+        color: #D9D9D9;
+        margin: 0;
+    
+`
+
+const TagButton =  styled.button`
+        font-size: 24px;
+        border-radius: 10px;
+        padding: 12px;
+        gap: 10px;
+        border: 2px solid transparent;
+        background: rgba(217, 217, 217, 0.3);
+        transition: background-color 0.3s ease; 
+        cursor: pointer;
+        color: #FFFFFF;       
+        box-sizing: border-box;
+        &:hover {
+      border-color: #C98CF1;
+    }
+       
+`
+
+
+const Tags = ({ setTag }) =>{
+        return(
+           <TagsContainer>
+                <TagTitulo>Busque por tags:</TagTitulo>
+                <div>
+                          {tags.map(tag => <TagButton key={tag.id} onClick={() => setTag(tag.tag)}>{tag.titulo}</TagButton>)}
+                         
+                </div>
+              
+           </TagsContainer>
+        )
+}
+
+
+export default Tags;
---
+++ src/componentes/Galeria/index.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import Titulo from "../Titulo";
+import Tags from "./Tags";
+import Populares from "./Populares";
+import Imagem from "./Imagem";
+
+const GaleriaContainer = styled.div`
+  display: flex;
+`;
+
+const SecaoFluida = styled.section`
+  flex-grow: 1;
+`;
+
+const ImagemContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 24px;
+`;
+
+const Galeria = ({ fotos = [], setTag, aoFotoSelecionada, aoAlternarFavorito }) => {
+  return (
+    <>
+      <Tags setTag={setTag} />
+      <GaleriaContainer>
+        <SecaoFluida>
+          <Titulo>Navegue pela galeria</Titulo>
+          <ImagemContainer>
+            {fotos.map((foto) => (
+              <Imagem
+                aoZoomSolicitado={aoFotoSelecionada}
+                aoAlternarFavorito={aoAlternarFavorito}
+                key={foto.id}
+                foto={foto}
+              />
+            ))}
+          </ImagemContainer>
+        </SecaoFluida>
+        <Populares />
+      </GaleriaContainer>
+    </>
+  );
+};
+
+export default Galeria;
